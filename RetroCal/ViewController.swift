82c395c51ff10478690fef2dca1e37e53f90d950
Kit Ho <kit.ho@iheha.com>
2016-11-17 17:45:29 +0800

    Initial Commit

---
+++ RetroCal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RetroCal
+//
+//  Created by Kit Ho on 17/11/2016.
+//  Copyright © 2016 Kit Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
